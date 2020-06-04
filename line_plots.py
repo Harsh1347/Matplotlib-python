04b1c2720b43d5233c5c5160baee4d770064f8fd
Harsh Gupta <harsh.gupta4@learner.manipal.edu>
2020-06-04 17:52:43 +0530

    first

---
+++ line_plots.py
@@ -0,0 +1,11 @@
+from matplotlib import pyplot as plt 
+
+x = [x for x in range(-3,4)]
+y = [x**2 for x in range(-3,4)]
+y2 = [x**3 for x in range(-3,4)]
+
+plt.style.use('fivethirtyeight')
+plt.plot(x,y,label = 'square')
+plt.plot(x,y2,label = 'cube')
+plt.legend()
+plt.show()
